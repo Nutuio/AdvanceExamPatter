69d7b88cb15c48d270c3426806793e2d63a5e63c
Saptarshi1456 <saptarshisinha1253@gmail.com>
2026-03-25 22:34:28 +0530

    Delete Python directory
    delete

--- Python/TwoNumbersGreaterThanEqual.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter a first number :"))
-b = int(input("Enter a second number :"))
-
-if a >=b:
-    print("a is greater than or equal to b")
-else:
-    print("a is less than b")
--- Python/TwoNumbersSmallerThanEqual.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter a first number :"))
-b = int(input("Enter a second number :"))
-
-if a <=b:
-    print("a is smaller than or equal to b")
-else:
-    print("a is greater than b")
